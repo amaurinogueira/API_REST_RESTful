a79501d0eb53b3934e6148b64225b0e3c9744aaf
Amauri Nogueira <50035391+amaurinogueira@users.noreply.github.com>
2020-09-02 15:03:38 -0300

    subindo server

---
+++ readme.md
@@ -0,0 +1,14 @@
+NUNCA deixar um cliente sem respostas
+STATUS RESPOSTA
+
+1xx: Informação;
+2xx: Sucesso;
+    200: OK
+    201: CREATED
+    204: Não tem conteúdo no POST PUT DELETE
+3xx: Redirection
+4xx: Client Error
+    400: Bad Request
+    404: Not Found
+5xx: Error Server
+    500: Internal Server Error
--- server.js
+++ server.js
@@ -1,6 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const data = require("./data.json");
+app.use(express.json());
 
+//GET Recebe os dados do resource
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+//GET Recebe os dados do resource
+app.get("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+
+    if(!client) return res.status(204).json();
+
+    res.json(client);
+});
+//POST Envia informações para ser processadas em um cliente para um resource
+app.post("/clients", function(req, res){
+    const { name, email } = req.body;
+
+    //lógica de salvar
+
+    res.json ({ name, email });
+
+});
+//PUT Atualiza dados do resrouce
+app.put("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+
+    if(!client) return res.status(204).json();
+
+    const { name } = req.body;
+
+    client.name = name;
+
+    res.json(client);
+
+});
+//DELETE Deleta um resource
+app.delete("/clients/:id", function(req, res){
+    const { id } = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+
+    res.json(clientsFiltered)
+
+});
+//Indica qual a porta vai se usada
 app.listen(3000, function(){
     console.log("Server is runing");
 });
